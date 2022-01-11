568310a872ee0e486cce60833046b6427e10a4f4
tim12367 <tim12367@ymail.com>
2022-01-11 20:22:46 +0800

    remove press space to start

--- detect.py
+++ detect.py
@@ -92,26 +92,23 @@ def run(model: str, camera_id: int, width: int, height: int, num_threads: int,
     cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
                 font_size, text_color, font_thickness)
 
-    # Stop the program if the ESC key is pressed.
-    if cv2.waitKey(1) == 27:
-      break
-
-    # Press Space to show the total_prices
-    elif cv2.waitKey(1) == 32:
-        # print(total_price)  # test on screen
-
-        # display on OLED
-        i2c = busio.I2C(board.SCL,board.SDA)
-        oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
-        oled.fill(0)
-        oled.text(f'Price = ', 0, 5, 1, size=2)
-        oled.text(f'    {total_price}', 0, 25, 1, size=2)
-        oled.show()
+    # display on OLED
+    i2c = busio.I2C(board.SCL,board.SDA)
+    oled = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C)
+    oled.fill(0)
+    oled.text(f'Price = ', 0, 5, 1, size=2)
+    oled.text(f'    {total_price}', 0, 25, 1, size=2)
+    oled.show()
 
     cv2.namedWindow('object_detector', 0)  # 0可調大小
     cv2.resizeWindow('object_detector', 400, 300)  # 設定長和寬
     cv2.imshow('object_detector', image)
 
+    # Stop the program if the ESC key is pressed.
+    if cv2.waitKey(1) == 27:
+        oled.fill(0)
+        oled.show()
+        break
 
 
   cap.release()
